a4969d674a471f1a36f9dffa51d34c4cd6fd37c7
ahmaddfauzi14 <ahmadfauzi7218@gmail.com>
2025-11-08 18:30:13 +0800

    Update: Memperbaiki tampilan fitur.

--- features/FPBdanKPK.py
+++ features/FPBdanKPK.py
@@ -1,9 +1,9 @@
 import re
-from utills import space, error, pembuka4, menu4
+from utills import space, error, pembuka4, menu4, info
 from InputValidation import validasi_data_input
 
 def hitung_fpb(a, b):
-    while b !=0:
+    while b != 0:
         a, b = b, a % b 
     return a
 
@@ -19,18 +19,23 @@ def eksekusi4():
                 break
             case 1:
                 while True:
-                    space()
+                    info("Ketik 'back' untuk kembali")
                     data_input = validasi_data_input(input("Masukkan Bilangan (Pisahkan Berdasarkan Spasi): "))
                     if not data_input:
                         continue
                     if data_input.lower() == "back":
                         break
                     try:
-                        pattern = r"^[0-9\s]+$"
+                        pattern = r"^[0-9\s-]+$"
                         result = re.match(pattern, data_input)
                         if result:
                             angka_str = data_input.split()
                             angka = [int(i) for i in angka_str]
+                            
+                            # Tambahkan logika penolakan angka negatif
+                            if any(i < 0 for i in angka):
+                                error("Tidak boleh ada bilangan negatif. Silakan coba lagi.")
+                                continue
                             
                             fpb = angka[0]
                             kpk = angka[0]
@@ -39,12 +44,15 @@ def eksekusi4():
                                 kpk = hitung_kpk(kpk, i)
                                 
                             space()
-                            print(f"FPB dari {angka} adalah {fpb}")
-                            print(f"KPK dari {angka} adalah {kpk}")
+                            print("+----------------------+-----------------+-----------------+")
+                            print("|       Bilangan       |       FPB       |       KPK       |")
+                            print("+----------------------+-----------------+-----------------+")
+                            print(f"| {str(angka):^20} | {fpb:^15} | {kpk:^15} |")
+                            print("+----------------------+-----------------+-----------------+")
                             break
+
                         else:
                             raise ValueError
                     except:
-                        error("Inputan harus berupa angka. Silahkan coba lagi.")
-                        continue 
-        
+                        error("Inputan harus berupa angka. Silakan coba lagi.")
+                        continue
